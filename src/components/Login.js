f8f3bcd6be1aeeb2b5545701d91e7ef1ad3ad3a6
Ebad11 <ebadshk48@gmail.com>
2024-07-29 12:43:48 +0530

    updated login url

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ function Login(props) {
     async function handleSubmit(e)
     {
         e.preventDefault();
-        const response = await fetch("https:notebook-app-backend-ebadshk.vercel.app/api/auth/login", {
+        const response = await fetch("https://notebook-app-backend-ebadshk.vercel.app/api/auth/login", {
             method: "POST", 
             headers: {
               "Content-Type": "application/json",
